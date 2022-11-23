d4ec9879db579a070d8fa25c7b4acfdbfb096349
davidpetkov <davidpetkov2006@gmail.com>
2022-11-23 18:53:16 +0200

    Added search bar on home screen.

--- EnvisionCreationsNew/EnvisionCreationsNew/Program.cs
+++ EnvisionCreationsNew/EnvisionCreationsNew/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -25,18 +24,16 @@ builder.Services.ConfigureApplicationCookie(options =>
 builder.Services.AddScoped<IUploadService, UploadService>();
 builder.Services.AddScoped<IDownloadService, DownloadService>();
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
